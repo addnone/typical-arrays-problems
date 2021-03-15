2b2120dd3c8b758f2b93bfe89cc0fffe001d6ecd
vsavin <savinvitya@gmail.com>
2021-03-15 16:49:24 +0300

    feat: add main logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if (Array.isArray(arguments[0])) {
+    if (array.length >= 1) {
+      result = array.reduce((acc, val) => {return val < acc ? val : acc}, array[0]);
+      return result
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(arguments[0])) {
+    if (array.length >= 1) {
+      result = array.reduce((acc, val) => { return val > acc ? val : acc}, array[0]);
+      return result
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(arguments[0])) {
+    if (array.length >= 1) {
+      let length = array.length
+      result = array.reduce((acc, val) => { return acc+val}, 0);
+      return result/length
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
 }
